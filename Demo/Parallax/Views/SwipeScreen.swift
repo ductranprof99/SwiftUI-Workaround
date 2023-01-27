274b705aeecacdcbf45463fc591d2ce14d5451ee
ductranprof99 <ductran.dev99@gmail.com>
2023-01-27 20:50:20 +0700

    Add swipe screen

--- Demo/Parallax/Views/SwipeScreen.swift
+++ Demo/Parallax/Views/SwipeScreen.swift
@@ -19,18 +19,95 @@ struct SwipeScreen: View {
     @AppStorage("isOnSwipeScreen") var swipeScreen: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
+        ZStack {
             
-            Text("Yo Yo Yo")
-                .font(.largeTitle)
-                .foregroundColor(.red)
+            Color("ParallaxBackground")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                swipeScreen = !swipeScreen
-            }) {
-                Text("Start")
-                    .font(.largeTitle)
-                    .foregroundColor(.red)
+            VStack(spacing: 20) {
+                
+                Spacer()
+                VStack (spacing: 0) {
+                    Text("Share")
+                        .fontWeight(.heavy)
+                        .font(.system(size: 60))
+                        .foregroundColor(Color("ParallaxTitleColor"))
+                    
+                    Text("this text here mean nothing, it is a place holder like loremm ispumm, and because its nnnnnnniggga ")
+                        .font(.title2)
+                        .fontWeight(.light)
+                        .padding(.horizontal, 20)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color("ParallaxTitleColor"))
+                }
+                
+                Spacer()
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("parallax-swipe")
+                        .resizable()
+                        .scaledToFit()
+                }
+                        
+                Spacer()
+                
+                ZStack {
+                    
+                    Text("Slide to unlock")
+                        .font(.system(.title3, design: .rounded))
+                        .offset(x: 20)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ParallaxSliderColor"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Capsule()
+                                .fill(Color("ParallaxSliderColor"))
+                            
+                            Capsule()
+                                .fill(.black.opacity(0.2))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .foregroundColor(.white)
+                                .font(.system(size: 24, weight: .medium))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        
+                        Spacer()
+                    }
+                    .onTapGesture {
+                        // Do sth here
+                    }
+                }
+                .frame(height: 80, alignment: .center)
+                .padding(40)
+                
+                
             }
         }
     }
